cd5f0b6f425affc40e12e8b8995f676c8bc34903
kminkyung <mminkyung.k@gmail.com>
2019-08-26 14:50:45 +0900

    memo contents design WIP

---
+++ public/js/note.js
@@ -0,0 +1,41 @@
+const auth = firebase.auth();
+const google = new firebase.auth.GoogleAuthProvider();
+var user = null; // 로그인한 사용자의 정보를 저장하는 변수
+
+//인증기능 만들기
+//$("#btLogin").click(function(e){});
+
+auth.onAuthStateChanged(data => {
+	user = data;
+	if(user == null) viewChg('');
+	else viewChg('R');
+});
+
+
+document.querySelector("#btLogin").addEventListener("click", e => {
+ auth.signInWithPopup(google);
+//  auth.signInWithRedirect(google);
+});
+document.querySelector("#btLogout").addEventListener("click", e => {
+	auth.signOut()
+});
+
+// 화면전환 함수
+function viewChg (state){
+	switch(state) {
+		case "R" :
+			imagesLoaded( document.querySelector('.email img'),() => {
+				document.querySelector(".email img").setAttribute("src", user.photoURL);
+				document.querySelector(".email-txt").innerHTML = user.email;
+				document.querySelector(".email").style.display = "flex";
+				document.querySelector("#btLogin").style.display = "none";
+			});
+			document.querySelector(".email img").setAttribute("src", user.photoURL);
+			break;
+		default :
+			document.querySelector(".email-txt").innerHTML = "";
+			document.querySelector(".email").style.display = "none";
+			document.querySelector("#btLogin").style.display = "inline-block";
+			break;
+	}
+}
